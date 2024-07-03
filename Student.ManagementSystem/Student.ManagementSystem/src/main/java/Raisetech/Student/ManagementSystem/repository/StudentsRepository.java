bda63ccbb8cbf345e1cba5132588b889ac8b5ff0
Ageji0808 <6120003@alumni.tus.ac.jp>
2024-07-03 19:27:53 +0900

    Add files via upload

---
+++ Student.ManagementSystem/Student.ManagementSystem/src/main/java/Raisetech/Student/ManagementSystem/repository/StudentsRepository.java
@@ -0,0 +1,26 @@
+package Raisetech.Student.ManagementSystem.repository;
+
+import Raisetech.Student.ManagementSystem.data.Students;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+
+
+
+  @Mapper
+  public interface StudentsRepository {
+
+
+
+  @Select("SELECT * FROM students")
+  List<Students> getAllStudents();
+
+
+
+
+
+  }
+
+
+
